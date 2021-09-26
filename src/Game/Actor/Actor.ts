a0c5b9fece1d4a4e6e1f5ad57827a0ebc5f4710b
decca-dev <medcherbani@gmail.com>
2021-09-27 00:07:38 +0100

    some stuff

--- src/Game/Actor/Actor.ts
+++ src/Game/Actor/Actor.ts
@@ -2,8 +2,12 @@ import { Vec } from "../Vec/Vec";
 import { Sprite } from "./Sprite";
 
 export abstract class Actor {
-  public pos!: Vec;
+  public pos: Vec;
   public sprite!: Sprite;
+
+  constructor(sprite: Sprite) {
+    this.pos = new Vec();
+  }
 
   public draw(): void {}
 
